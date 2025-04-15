364d9430295373a8fec23d8981048046c29b5655
Demetri Manias <demetrimanias@gmail.com>
2025-04-15 18:33:56 +0300

    Changes to Print Lease PDF function DM (2025-04-15)

--- mysite/settings.py
+++ mysite/settings.py
@@ -160,7 +160,8 @@ STATICFILES_DIRS = [
 
 # Push site online
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- print_lease.py
+++ print_lease.py
@@ -5,6 +5,7 @@ def lease_report (property, rep_output, email, fname):
 	import pdf_display
 	import send_email
 	from django.conf import settings
+	from django.templatetags.static import static
 
 	# CONNECT TO DATBASE (FIRST HAVE TO LEAVE database line off until have created database)
 	mydb = mysql.connector.connect(
@@ -21,28 +22,10 @@ def lease_report (property, rep_output, email, fname):
 
 	today=date.today()
 
-	# import fpdf
-	# pdf = fpdf.FPDF(format='A4')
-	
-	# pdf.add_page()
-	# pdf.set_font("Arial", "BU", size=20)
-	# pdf.cell(w=0,h=8, txt="TITLE DEED REPORT - (" + str(today) + ")", align="C")
-	# pdf.set_font("Arial", "BU", size=20)
-	# pdf.write (5,"Property Details Report")
-	# pdf.set_font("Arial", "B", size=16)
-	# pdf.write (5,f" (Date: {today})")
-	# pdf.ln()
-	# pdf.ln()
+#	file_path = "C:/Users/DemetrisManias/Desktop/code/djangoproject/static/lease_agreements/"
+#	report_name = file_path+property+" - Lease Agreement.pdf"
 
-	# pdf.set_font("Arial", size=14)
-	# pdf.set_left_margin (10)
-
-	# my_cursor.execute("SELECT * FROM prop ORDER BY prop.prop_country ASC, prop.prop_name ASC")
-	# result = my_cursor.fetchall()
-
-	file_path = "C:/Users/DemetrisManias/Desktop/code/djangoproject/static/lease_agreements/"
-
-	report_name = file_path+property+" - Lease Agreement.pdf"
+	file_path = os.path.join(settings.STATIC_ROOT,property," - Lease Agreement.pdf")
 	
 	# send email to (email address, email subject, report name and "file path and name") - fixed body text for email
 	if rep_output == "Email":
