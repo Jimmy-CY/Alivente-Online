ce277aecb7ae3e6534a0acf26508f7b5946b87dd
Demetri Manias <demetrimanias@gmail.com>
2025-04-16 08:33:34 +0300

    Changes to Print Lease PDF function and env DM (2025-04-16)

--- print_lease.py
+++ print_lease.py
@@ -4,37 +4,43 @@ def lease_report (property, rep_output, email, fname):
 	from datetime import date
 	import pdf_display
 	import send_email
+	import os
 	from django.conf import settings
 	from django.templatetags.static import static
 
-	# CONNECT TO DATBASE (FIRST HAVE TO LEAVE database line off until have created database)
-	mydb = mysql.connector.connect(
-		host = settings.DATABASES['default']['HOST'],
-		port = settings.DATABASES['default']['PORT'],
-		user = settings.DATABASES['default']['USER'],
-		password = settings.DATABASES['default']['PASSWORD'],
-		database = settings.DATABASES['default']['NAME'],
-		auth_plugin = settings.DATABASES['default']['AUTH_PLUGIN'],
-	)
 
-	# CREATE CURSOR INSTANCE
-	my_cursor = mydb.cursor()
+	mydb = None
+	try:
+		# CONNECT TO DATBASE (FIRST HAVE TO LEAVE database line off until have created database)
+		mydb = mysql.connector.connect(
+			host = settings.DATABASES['default']['HOST'],
+			port = settings.DATABASES['default']['PORT'],
+			user = settings.DATABASES['default']['USER'],
+			password = settings.DATABASES['default']['PASSWORD'],
+			database = settings.DATABASES['default']['NAME'],
+			auth_plugin = settings.DATABASES['default']['AUTH_PLUGIN'],
+		)
 
-	today=date.today()
+		# CREATE CURSOR INSTANCE
+		my_cursor = mydb.cursor()
+
+		today=date.today()
 
 #	file_path = "C:/Users/DemetrisManias/Desktop/code/djangoproject/static/lease_agreements/"
 #	report_name = file_path+property+" - Lease Agreement.pdf"
 
-	file_path = os.path.join(settings.STATIC_ROOT,property," - Lease Agreement.pdf")
-	
-	# send email to (email address, email subject, report name and "file path and name") - fixed body text for email
-	if rep_output == "Email":
-		send_email.send_email(email,property+" - Lease Agreement",property+" - Lease Agreement",report_name,fname)
+		report_name = os.path.join(settings.STATIC_ROOT, property + " - Lease Agreement.pdf")
+		
+		# send email to (email address, email subject, report name and "file path and name") - fixed body text for email
+		if rep_output == "Email":
+			send_email.send_email(email, property + " - Lease Agreement", property + " - Lease Agreement", report_name, fname)
 
-	# display pdf file in new window - send file_name
-	if rep_output == "Display":
-		pdf_display.pdf_display(report_name)
+		# display pdf file in new window - send file_name
+		if rep_output == "Display":
+			pdf_display.pdf_display(report_name)
 
-	if mydb.is_connected():
-		my_cursor.close()
-		mydb.close()
+	finally:
+		# Ensure resources are closed
+		if mydb is not None and mydb.is_connected():
+			my_cursor.close()
+			mydb.close()
